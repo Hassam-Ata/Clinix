10ccd7bbd7792dfe2af63c4ea2b7027505530672
Hassam-Ata <hassamata86@gmail.com>
2026-04-20 00:53:33 +0500

    init:initialized frontend with shadcn

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <Button variant="outline">Button</Button>
+    </div>
+  );
+};
+
+export default App;
